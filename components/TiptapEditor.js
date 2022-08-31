6be1d287b027aacbb0762c7e011b50800f0e325a
Dongw1126 <topblade71@naver.com>
2022-08-31 17:43:43 +0900

    feat: content 로깅 추가

--- components/TiptapEditor.js
+++ components/TiptapEditor.js
@@ -1,6 +1,10 @@
 import {EditorContent, useEditor} from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import React from 'react'
+// https://tiptap.dev/installation/nextjs
+
+
+const mockData = '<h1>테스트</h1><p>테스트 입니다</p>';
 
 const MenuBar = ({editor}) => {
     if (!editor) {
@@ -121,47 +125,19 @@ const MenuBar = ({editor}) => {
     )
 }
 
-export default function TiptapEditor() {
+export default function TiptapEditor({ onClick }) {
     const editor = useEditor({
         extensions: [
             StarterKit,
         ],
-        content: `
-      <h2>
-        Hi there,
-      </h2>
-      <p>
-        this is a <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
-      </p>
-      <ul>
-        <li>
-          That’s a bullet list with one …
-        </li>
-        <li>
-          … or two list items.
-        </li>
-      </ul>
-      <p>
-        Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
-      </p>
-      <pre><code class="language-css">body {
-  display: none;
-}</code></pre>
-      <p>
-        I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
-      </p>
-      <blockquote>
-        Wow, that’s amazing. Good work, boy! 👏
-        <br />
-        — Mom
-      </blockquote>
-    `,
+        content: mockData,
     })
 
     return (
         <div>
             <MenuBar editor={editor}/>
             <EditorContent editor={editor}/>
+            <button onClick={() => onClick(editor)}>로깅</button>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,18 @@ import styles from '../styles/Home.module.css'
 import TiptapEditor from "../components/TiptapEditor";
 
 export default function Home() {
-  return (
-      <div>
-        <main>
-          <div>
-            <h1>에디터 테스트</h1>
-            <div>
-              <TiptapEditor/>
-            </div>
-          </div>
-        </main>
-      </div>
-  )
+    const onClick = (editor) => console.log(editor.getHTML());
+
+    return (
+        <div>
+            <main>
+                <div>
+                    <h1>에디터 테스트</h1>
+                    <div style={{margin: "1.2rem"}}>
+                        <TiptapEditor onClick={onClick}/>
+                    </div>
+                </div>
+            </main>
+        </div>
+    )
 }
